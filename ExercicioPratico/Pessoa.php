34911a54a67b3392bb66cc51609ef51b16a5a0fc
Vinicius Terriani <viniterriani.vt@gmail.com>
2018-08-16 12:48:51 -0300

    classe Pessoa completa

---
+++ ExercicioPratico/Pessoa.php
@@ -0,0 +1,45 @@
+<?php 
+
+class Pessoa{
+
+    //atributos 
+    private $nome;
+    private $idade;
+    private $sexo;
+
+    //construtor da classe
+    public function Pessoa($nome, $idade, $sexo){
+        $this->nome = $nome;
+        $this->idade = $idade;
+        $this->sexo = $sexo;
+    }
+
+    //metodos especiais
+
+    public function getNome()
+    {
+    return $this->nome;
+    }
+
+    public function getIdade()
+    {
+    return $this->idade;
+    }
+
+    public function setIdade($idade)
+    {
+    $this->idade = $idade;
+
+    return $this;
+    }
+
+    public function getSexo()
+    {
+    return $this->sexo;
+    }
+
+    //metodos da classe Pessoa
+    public function fazerAniversario(){
+        $this->setIdade( $this->getIdade() + 1 );
+    }
+}
