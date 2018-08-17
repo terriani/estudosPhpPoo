d4360ea15ad4d489a8bc9800d789ba5c919080e2
Vinicius Terriani <viniterriani.vt@gmail.com>
2018-08-17 20:25:23 -0300

    comentario  lembrete na inhdex

--- herança/index.php
+++ herança/index.php
@@ -4,8 +4,8 @@ require_once "Aluno.php";
 require_once "Funcionario.php";
 require_once "Professor.php";
 
-$a = new Aluno("joao", 18, "masc");
 
-$a1 = new Aluno("Joao2", 20, "masc", 111, "si");
+//criar pessoas e testar metodos
+$a = new Aluno("joao", 18, "masc");
 
 print_r($a1);
