a43a3ffd8121f6e40e0a7fe1fcb6c9387f58f118
Vinicius Terriani <viniterriani.vt@gmail.com>
2018-08-19 10:33:51 -0300

    implementando a classe mamifero

---
+++ Polimorfismo/Mamifero.php
@@ -0,0 +1,35 @@
+<?php 
+
+require_once "Animal.php";
+
+class Mamifero extends Animal{
+
+    private $corPelo;
+
+    
+
+    /**
+     * Get the value of corPelo
+     */ 
+    public function getCorPelo()
+    {
+        return $this->corPelo;
+    }
+
+    /**
+     * Set the value of corPelo
+     *
+     * @return  self
+     */ 
+    public function setCorPelo($corPelo)
+    {
+        $this->corPelo = $corPelo;
+
+        return $this;
+    }
+
+    //metodos sobrescritos da classe Aanimal
+    public function locomover(){
+        
+    }
+}
