0b134b85a711966cabc6157aef170a14e20841b7
Vinicius Terriani <viniterriani.vt@gmail.com>
2018-08-18 22:56:23 -0300

    getters e setters da classe pessoa e o metodo fazer aniversario implementado

--- ArvoreHeranca/Pessoa.php
+++ ArvoreHeranca/Pessoa.php
@@ -4,9 +4,71 @@ abstract class  Pessoa{
 
     //atributos
     private $nome;
-    private $idade;
+    private $idade = 0;
     private $sexo;
 
-    
+    final public function fazerNiver(){
+        $this->setIdade($this->getIdade() + 1);
+    }
 
+
+    /**
+     * Get the value of nome
+     */ 
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * Set the value of nome
+     *
+     * @return  self
+     */ 
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of idade
+     */ 
+    public function getIdade()
+    {
+        return $this->idade;
+    }
+
+    /**
+     * Set the value of idade
+     *
+     * @return  self
+     */ 
+    public function setIdade($idade)
+    {
+        $this->idade = $idade;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of sexo
+     */ 
+    public function getSexo()
+    {
+        return $this->sexo;
+    }
+
+    /**
+     * Set the value of sexo
+     *
+     * @return  self
+     */ 
+    public function setSexo($sexo)
+    {
+        $this->sexo = $sexo;
+
+        return $this;
+    }
 }
