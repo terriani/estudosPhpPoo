362350f57a3a7bde46a6c7f9ef4741f11157372b
Vinicius Terriani <viniterriani.vt@gmail.com>
2018-08-23 21:40:47 -0300

    implementando o index do exercicio de sessões

---
+++ sessoes2/index.php
@@ -0,0 +1,19 @@
+
+<?php
+require_once "lib/autoload.php";
+$session = new Session();
+$session->getSession('teste');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
