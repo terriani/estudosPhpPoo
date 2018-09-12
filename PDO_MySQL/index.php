813e06915558b909dec6e8b8619e0d93cf5adbe4
Vinicius Terriani <viniterriani.vt@gmail.com>
2018-09-12 00:12:28 -0300

    criando primeira conexão com banco de dados em pdo

---
+++ PDO_MySQL/index.php
@@ -0,0 +1,111 @@
+<?php
+
+
+//cria uma sring de conexão passando o driver do banco o nome do banco e o charset
+$dns = 'mysql:dbname=estudos_PDO;charset=utf8';
+
+//usuario 
+$user = "root";
+
+//senha
+$pass = "";
+
+//configurações inicias do pdo
+$options = [
+
+  //testa se já existe uma conexão identica caso já exixta o pdo se conecta por ela não abrindo uma nova conexão  
+  PDO::ATTR_PERSISTENT => true,
+
+    //transfoma todo erro do mysql em uma exception
+  PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+
+    //define como padrao o fetch_object (fetch_obj transforma os dados vindos do banco em objetos)
+  PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
+
+];
+
+//tenta executar a conexão com o banco de dados informado 
+try{
+
+    //executa a conexão
+    $pdo = new PDO($dns, $user, $pass, $options);
+
+
+}
+
+//caso de erro na conexão executa  uma msg de erro
+catch(PDOException $e){
+
+  //exibe a msg de erro (getMesssage() pega a msg de erro vinda do PDOException)
+    exit('ERR' . $e->getMessage());
+
+}
+
+
+//insere um funcionario na tabela para teste
+//$insert = $conn->query('INSERT INTO funcionarios (nome, idade, email) VALUES ("joao", 22, "joao@j.com")');
+
+
+//executa uma busca no banco com pdo
+$stmt = $pdo->query('SELECT id, nome, idade, email FROM funcionarios');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+      <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <title>Title of the document</title>
+    <!-- Material Icon CDN -->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!-- Materialize CSS CDN -->
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+    <!-- Your custom styles -->
+      <link rel="stylesheet" href="css/style.css">
+    <!-- Used as an example only to position the footer at the end of the page.
+    You can delete these styles or move it to your custom css file -->
+    <style>
+      body {
+        display: flex;
+        min-height: 100vh;
+        flex-direction: column;
+        }
+      main {
+        flex: 1 0 auto;
+      }
+    </style>
+  </head>
+  <body>
+   <table class="centered striped">
+       <thead>
+           <tr>
+               <th data-field="id">id</th>
+               <th data-field="name">nome</th>
+               <th data-field="price">idade</th>
+               <th data-field="email">email</th>
+           </tr>
+       </thead>
+       <tbody>
+           <?php foreach($stmt as $employee):;?>
+           <tr>
+               <td><?= $employee->id; ?></td>
+               <td><?= $employee->nome; ?></td>
+               <td><?= $employee->idade; ?></td>
+               <td><?= $employee->email?></td>
+            </tr>
+            <? endforeach ?>
+       </tbody>
+   </table>
+    <!-- jQuery CDN -->
+      <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+    <!-- Materialize JS CDN -->
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+    <script>
+      $("document").ready(function(){
+        $(".button-collapse").sideNav();
+      });
+    </script>
+  </body>
+</html>
